acf0c01b59ea4ce57fdbe5e1d082b92038b53bbf
Wisdom Ojimah <vindicatedwisdom@gmail.com>
2023-04-03 00:10:30 +0100

    feat: custom controller to get individual category details by slug instead of id

--- config/plugins.js
+++ config/plugins.js
@@ -19,4 +19,8 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  // ckeditor5: {
+  //   enabled: true,
+  //   resolve: "./src/plugins/strapi-plugin-ckeditor"
+  // },
 });
--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -1,9 +1,25 @@
-'use strict';
+"use strict";
 
 /**
  * category controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::category.category');
+// module.exports = createCoreController('api::category.category');
+
+module.exports = createCoreController(
+  "api::category.category",
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+
+      const entity = await strapi.db.query("api::category.category").findOne({
+        where: { slug: id },
+      });
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
